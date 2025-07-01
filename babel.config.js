6f66615bd4e596b821c25d967ad666b667a815a8
Arnav Raj <arnavrajcodes@gmail.com>
2025-07-01 15:40:25 +0530

    babel config

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-      [
-        "module-resolver",
-        {
-          alias: {
-            "@": "./app", // ✅ or './' if assets/constants are in root
-          },
-        },
-      ],
     ],
   };
 };
