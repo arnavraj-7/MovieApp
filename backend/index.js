1581f4757abde32abc18afe3c6159e14955dfdc6
Arnav Raj <arnavrajcodes@gmail.com>
2025-07-02 02:13:54 +0530

    setup cors

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,25 @@ import express from 'express';
 import { configDotenv } from 'dotenv';
 configDotenv();
 import CORS from 'cors';
+import router from './routes/fetch.routes.js';
+import connectDB from './utils/db.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(CORS())
-app.use(JSON.parse())
+app.use(CORS(
+    {
+        origin:'*',
+    }
+))
+app.use(express.json());
 
-app.route('/',)
 
-app.listen(PORT, () => {
+app.use('/',router)
+
+app.listen(PORT,async() => {
+    await connectDB();
     console.log(`Server is running on port ${PORT}`);
 });
 
