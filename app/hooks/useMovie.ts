77f215a80ed54dde10f11301f071846f17a13c25
Arnav Raj <arnavrajcodes@gmail.com>
2025-07-02 02:14:48 +0530

    changed fetch url

--- app/hooks/useMovie.ts
+++ app/hooks/useMovie.ts
@@ -1,42 +1,14 @@
-import axios from "axios";
+import axios from 'axios'
 
 
-const options = {
-  method: "GET",
-  url: "https://ott-details.p.rapidapi.com/advancedsearch",
-  params: {
-    start_year: "1970",
-    end_year: "2025",
-    min_imdb: "6",
-    max_imdb: "10",
-    genre: "action",
-    language: "english",
-    type: "movie",
-    sort: "latest",
-    page: "1",
-  },
-  headers: {
-    "x-rapidapi-key": "fd44cbdd95msh5ca79868ff3c831p1e21afjsn4b688424d236",
-    "x-rapidapi-host": "ott-details.p.rapidapi.com",
-  },
-};
 
-
-const useMovies = ()=>{
- let data : any = []
+const useMovies = () => {
   const fetchMovies = async () => {
-    try {
-      const response = await axios.request(options);
-      console.log(response.data);
-      data = response.data;
-    } catch (error) {
-      console.error(error);
-    }
+    console.log("hello");
+    const response  = await axios.get('http://192.168.29.210:8001/fetch');
+    console.log(response);
+    return response.data;
   }
-  return {
-    fetchMovies,data
-  }
-};
-
-
-export default useMovies;
+  return {fetchMovies}
+}
+export default useMovies
