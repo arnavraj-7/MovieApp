60a9b2374ef760c48c5aeed171795d30e7204401
Arnav Raj <arnavrajcodes@gmail.com>
2025-07-03 00:52:43 +0530

    hook with fetchbyId

--- app/hooks/useMovie.ts
+++ app/hooks/useMovie.ts
@@ -2,13 +2,26 @@ import axios from 'axios'
 
 
 
-const useMovies = () => {
-  const fetchMovies = async () => {
+const useAnime = () => {
+  const fetchTop = async () => {
     console.log("hello");
-    const response  = await axios.get('http://192.168.29.210:8001/fetch');
+    const response  = await axios.get('http://192.168.29.210:8001/top');
     console.log(response);
     return response.data;
   }
-  return {fetchMovies}
+  const fetchUpcoming = async () => {
+    console.log("hello");
+    const response  = await axios.get('http://192.168.29.210:8001/upcoming');
+    console.log(response);
+    return response.data;
+  }
+  const fetchbyId = async (id) => {
+    // console.log("hello");
+    const response  = await axios.get(`http://192.168.29.210:8001/${id}`);
+    console.log(response);
+    return response.data;
+  }
+
+  return {fetchTop,fetchUpcoming,fetchbyId}
 }
-export default useMovies
+export default useAnime
