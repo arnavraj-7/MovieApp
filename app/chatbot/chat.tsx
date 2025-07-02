ea6a9bbbc1002f60f7894e81045d644ff800b8c4
Arnav Raj <arnavrajcodes@gmail.com>
2025-07-03 04:10:45 +0530

    rough testing ui for gemini

--- app/chatbot/chat.tsx
+++ app/chatbot/chat.tsx
@@ -1,63 +1,67 @@
-import { View, Text, ScrollView, TextInput } from 'react-native'
-import React, { useState } from 'react'
-import axios from 'axios'
+import { View, Text, ScrollView, TextInput } from "react-native";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Chat = () => {
-    const handleSend=async(message:string)=>{
-        setMessages((prev)=>{
-            if(prev===undefined){
-                return {
-                    human:[message],bot:[
-                        
-                    ]
-                }
-            }
-            return {
-                human:[...prev.human,message],bot:[...prev.bot]
-            }
-        })
-        const res = await axios.post('http://192.168.29.210:8001/chat',messages);
-        console.log(res.data);
-        setMessages((prev)=>{
-            if(prev===undefined){
-                return {
-                    human:[message],bot:[
-                        
-                    ]
-                }
-            }
-            return {
-                human:[...prev.human],bot:[...prev.bot,res.data]
-            }
-        })
-    }
-  const [messages, setMessages] = useState<{human:string[],bot:string[]}>();
-  return (
-    <View className='flex-1 bg-black'
-    style={{
-        paddingTop:50,
-        paddingHorizontal:20
-    }}
-    >
-      <Text className='text-white'>Chat about anime</Text>
-      <View>
-    <ScrollView className='pb-10 px-4'>
+  const handleSend = async (message: string) => {
+    setMessages((prev) => {
+      if (prev === undefined) {
+        return [{ role: "user", content: message }];
+      }
+      return [...prev, { role: "user", content: message }];
+    });
+    const res = await axios.post("http://192.168.29.210:8001/chat", {
+      messages: [...message, { role: "user", content: message }],
+    });
+    console.log(res.data.response);
 
-    </ScrollView>
+    setMessages((prev) => {
+      if (prev === undefined) {
+        return [{ role: "assistant", content: res.data.response }];
+      }
+      return [...prev, { role: "assistant", content: res.data.response }];
+    });
+  };
+  const [messages, setMessages] = useState<{ role: string; content: string }[]>(
+    []
+  );
+  return (
+    <View
+      className="flex-1 bg-black"
+      style={{
+        paddingTop: 50,
+        paddingHorizontal: 20,
+      }}
+    >
+      <Text className="text-white">Chat about anime</Text>
+      <View>
+        <ScrollView className="pb-10 px-4">
+          {messages.map((message, index) => {
+            return (
+              <View className="p-4 bg-slate-700 rounded-lg mb-2" key={index}>
+                <Text className={`text-white`}>
+                  {message.role + " : "} {message.content}
+                </Text>
+              </View>
+            );
+          })}
+        </ScrollView>
       </View>
       <TextInput
-
-      placeholder='Type your message here'
-      onSubmitEditing={handleSend}
-      style={{
-        position:'absolute',
-        bottom:240,
-        width:'100%'
-      }}
-      className='bg-gray-700 px-4 py-2 rounded-full text-white'
+        placeholder="Type your message here"
+        onSubmitEditing={(e) => {
+          e.preventDefault();
+          handleSend(e.nativeEvent.text);
+        }}
+        style={{
+          position: "absolute",
+          bottom: 240,
+          width: "100%",
+        }}
+        className="bg-gray-700 px-4 py-2 rounded-full text-white"
       />
     </View>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
