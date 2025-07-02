0f9d23197a94c233f38dad665d60cba8fdcca5d1
Arnav Raj <arnavrajcodes@gmail.com>
2025-07-03 00:51:38 +0530

    controller for anime* by id

--- backend/controllers/fetch.controller.js
+++ backend/controllers/fetch.controller.js
@@ -1,5 +1,4 @@
-// import Movie from "../models/movies.schema.js";
-
+import axios from 'axios';
 // const fetchMovies = async(req,res)=>{
 //     try{
 //         const movies = await Movie.find({});
@@ -11,12 +10,44 @@
 // }
 // export default fetchMovies
 
-const fetchMovies = async () => {
+const fetchTop = async (req,res) => {
     try {
-        const res = await axios.get('https://api.jikan.moe/v4/anime');
-        console.log(res.data);
-        return res.data
+        const anime = await axios.get('https://api.jikan.moe/v4/top/anime');
+        console.log(anime.data);
+        res.status(200).json(anime.data.data);
+        // res.status(200).json({"message":"Success"})
     } catch (error) {
+        res.status(500).json({"error":error.message})
         console.log(error);
     }
 }
+const fetchUpcoming = async (req,res) => {
+    try {
+        const anime = await axios.get('https://api.jikan.moe/v4/seasons/upcoming');
+        console.log(anime.data);
+        res.status(200).json(anime.data.data);
+        // res.status(200).json({"message":"Success"})
+    } catch (error) {
+        res.status(500).json({"error":error.message})
+        console.log(error);
+    }
+}
+const fetchbyId = async (req,res) => {
+  try {
+    const {id} = req.params;
+    console.log("id:",id);
+    console.log("url:",`https://api.jikan.moe/v4/anime//full`);
+      const anime = await axios.get(`https://api.jikan.moe/v4/anime/${id}/full`);
+      console.log(anime.data);
+      res.status(200).json(anime.data.data);
+      // res.status(200).json({"message":"Success"})
+  } catch (error) {
+      res.status(500).json({"error":error.message})
+      console.log(error.message);
+  }
+}
+
+
+
+
+export  {fetchTop,fetchUpcoming,fetchbyId}
