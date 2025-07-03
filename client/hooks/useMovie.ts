0e35701028b7b5ef366db20f2fe693fadeb573cc
Arnav Raj <arnavrajcodes@gmail.com>
2025-07-04 03:09:15 +0530

    changed url of backend

--- client/hooks/useMovie.ts
+++ client/hooks/useMovie.ts
@@ -4,39 +4,42 @@ import axios from 'axios'
 
 
 const useAnime = () => {
+  let url = '';
+  process.env.MODE!="dev"? url='https://animeverse-awjb.onrender.com': url='http://192.168.29.210:8001';
   const fetchRandom = async () => {
+
     // console.log("hello");
-    const response  = await axios.get('http://192.168.29.210:8001/random');
+    const response  = await axios.get(`${url}/random`);
     // console.log(response);
     return response.data;
   }
   const fetchTop = async () => {
     // console.log("hello");
-    const response  = await axios.get('http://192.168.29.210:8001/top');
+    const response  = await axios.get(`${url}/top`);
     console.log("top fetched");
     return response.data;
   }
   const fetchUpcoming = async () => {
     // console.log("hello");
-    const response  = await axios.get('http://192.168.29.210:8001/upcoming');
+    const response  = await axios.get(`${url}/upcoming`);
     console.log("upcoming fetched");
     return response.data;
   }
   const fetchbyId = async (id:string) => {
     // console.log("hello");
-    const response  = await axios.get(`http://192.168.29.210:8001/anime/${id}`);
+    const response  = await axios.get(`${url}/anime/${id}`);
     // console.log(response);
     return response.data;
   }
   const fetchManga = async () => {
     // console.log("hello");
-    const response  = await axios.get(`http://192.168.29.210:8001/manga`);
+    const response  = await axios.get(`${url}/manga`);
     // console.log(response);
     return response.data;
   }
   const fetchMangabyId = async (id:string) => {
     // console.log("hello");
-    const response  = await axios.get(`http://192.168.29.210:8001/manga/${id}`);
+    const response  = await axios.get(`${url}/manga/${id}`);
     // console.log(response);
     return response.data;
   }
